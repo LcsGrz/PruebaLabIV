b1a586b903640e161a22b4843d3cc53970bc8e44
Gustavo David Riveros <gdrgus@gmail.com>
2018-04-02 20:47:23 -0300

    promesas crawler riveros molina

--- GustavoRiverosCrawler/index.js
+++ GustavoRiverosCrawler/index.js
@@ -1,8 +1,8 @@
-const request = require ('request');
+const request = require('request');
 const cheerio = require('cheerio');
-const i=0;
+/*const i = 0;
 
-/*request.post({url: 'http://noticias.ulp.edu.ar/php/includes/autoload_process.php', form: { 'group_no': 0 }}, (err, response, html) => {
+request.post({url: 'http://noticias.ulp.edu.ar/php/includes/autoload_process.php', form: { 'group_no': 0 }}, (err, response, html) => {
 	if (err) { return console.log(err); }
 	const $ = cheerio.load(html);
 	$('div.titulo-portada a').each (function (i, elemento) {
@@ -11,117 +11,45 @@ const i=0;
 		console.log(links);
 		console.log(a.text());
 	})
-});*/
+});
 
-const crawler = (callback) =>{
-	const arre= new Array();
-	request.post({url: 'http://noticias.ulp.edu.ar/php/includes/autoload_process.php', form: { 'group_no': 0 }}, (err, response, html) => {
-		if (err) { return console.log(err); }
-		const $ = cheerio.load(html);
-		$('div.titulo-portada a').each (function (i, elemento) {
-			const a = $(this);
-			const links = a.attr('href');
-			//console.log(links);
-			//console.log(i);
-			arre[i]={ detalle: links, titulo: a.text().trim() };
-			i++;
-			//console.log(a.text());
-		})
-		callback(arre)
-	});
+const crawler = (callback) => {
+    const arre = new Array();
+    request.post({ url: 'http://noticias.ulp.edu.ar/php/includes/autoload_process.php', form: { 'group_no': 0 } }, (err, response, html) => {
+        if (err) { return console.log(err); }
+        const $ = cheerio.load(html);
+        $('div.titulo-portada a').each(function (i, elemento) {
+            const a = $(this);
+            const links = a.attr('href');
+            //console.log(links);
+            //console.log(i);
+            arre[i] = { detalle: links, titulo: a.text().trim() };
+            i++;
+            //console.log(a.text());
+        })
+        callback(arre)
+    });
 }
 
 crawler((arre) => console.log(arre))
-
-
-/*const crawler = (callback) => new Promise(exito,err){
-	const arre= new Array();
-	request.post(
-		{
-			url: 'http://noticias.ulp.edu.ar/php/includes/autoload_process.php', 
-			form: { 'group_no': 0 }
-		}, 
-		(e, response, html) => 
-		{
-			e ? err(e) : 
-			
-			const $ = cheerio.load(html);
-			$('div.titulo-portada a').each (function (i, elemento) {
-				const a = $(this);
-				const links = a.attr('href');
-				//console.log(links);
-				//console.log(i);
-				arre[i]={ detalle: links, titulo: a.text().trim() };
-				i++;
-			})
-			exito(arre);
-		}
-	);
+*/
+const CrawlerPromesa = (pagina, seccion) => {
+    return new Promise((resolve, reject) => {
+        request.post(pagina, (err, res, body) => {
+            if (err) { reject(error); }
+            const $ = cheerio.load(body)
+            const data = $(seccion).text()
+            resolve(data)
+        })
+    })
 }
 
-Promise.crawler.then(data => qwrite(
-	console.log(data);
-)*/
+CrawlerPromesa("http://www.lapuntasanluis.com/", 'h2.article-title')
+.then(data => console.log(data))
+.catch(error => console.error(error))
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
