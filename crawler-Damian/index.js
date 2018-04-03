c7bbd037d35d7b54cc564d9bd96eb8d6933295e5
dvillarreal1234 <villarrealdamian@hotmail.com.ar>
2018-04-02 22:58:46 -0300

    'daleeeeee'

--- crawler-Damian/index.js
+++ crawler-Damian/index.js
@@ -1,8 +1,7 @@
-const request = require ('request');
+const request = require('request');
 const cheerio = require('cheerio');
-const i=0;
-
-/*request.post({url: 'http://noticias.ulp.edu.ar/php/includes/autoload_process.php', form: { 'group_no': 0 }}, (err, response, html) => {
+/*const i = 0;
+request.post({url: 'http://noticias.ulp.edu.ar/php/includes/autoload_process.php', form: { 'group_no': 0 }}, (err, response, html) => {
 	if (err) { return console.log(err); }
 	const $ = cheerio.load(html);
 	$('div.titulo-portada a').each (function (i, elemento) {
@@ -11,27 +10,40 @@ const i=0;
 		console.log(links);
 		console.log(a.text());
 	})
-});*/
-
-const crawler = (callback) =>{
-	const arre= new Array();
-	request.post('http://noticias.ulp.edu.ar/php/functions/functions.php?operacion=7', (err, response, html) => {
-		if (err) { return console.log(err); }
-		const $ = cheerio.load(html);
-		$('div.titulo-portada a').each (function (i, elemento) {
-			const a = $(this);
-			const links = a.attr('href');
-			//console.log(links);
-			//console.log(i);
-			arre[i]={ detalle: links, titulo: a.text().trim() };
-			i++;
-			//console.log(a.text());
-		})
-		callback(arre)
-	});
+});
+const crawler = (callback) => {
+    const arre = new Array();
+    request.post({ url: 'http://noticias.ulp.edu.ar/php/includes/autoload_process.php', form: { 'group_no': 0 } }, (err, response, html) => {
+        if (err) { return console.log(err); }
+        const $ = cheerio.load(html);
+        $('div.titulo-portada a').each(function (i, elemento) {
+            const a = $(this);
+            const links = a.attr('href');
+            //console.log(links);
+            //console.log(i);
+            arre[i] = { detalle: links, titulo: a.text().trim() };
+            i++;
+            //console.log(a.text());
+        })
+        callback(arre)
+    });
+}
+crawler((arre) => console.log(arre))
+*/
+const CrawlerPromesa = (pagina, seccion) => {
+    return new Promise((resolve, reject) => {
+        request.post(pagina, (err, res, body) => {
+            if (err) { reject(error); }
+            const $ = cheerio.load(body)
+            const data = $(seccion).text()
+            resolve(data)
+        })
+    })
 }
 
-crawler((arre) => console.log(arre))
+CrawlerPromesa("http://www.sanluisinforma.com.ar/", 'h2.article-title')
+.then(data => console.log(data))
+.catch(error => console.error(error))
 
 
 
