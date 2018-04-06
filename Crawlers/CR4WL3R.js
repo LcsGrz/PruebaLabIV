d8662fc855c7a0c5f92ad4128e6aba7832df67d6
LcsGrz <lcsjuegos@gmail.com>
2018-04-06 18:50:37 -0300

    Escribe JSON

--- Crawlers/CR4WL3R.js
+++ Crawlers/CR4WL3R.js
@@ -6,53 +6,56 @@ const diarios = {
 		url: 'http://noticias.ulp.edu.ar/php/functions/functions.php?operacion=7',
 		patron: 'div#titulo-portada'
 	},
-	diariosl:{
+	diariosl: {
 		url: "http://eldiariodesanluis.com/",
 		patron: 'h3 a.title'
 	},
-	slinforma:{
-        url: "http://www.sanluisinforma.com.ar/",
-        diario: "San Luis Informa",
+	slinforma: {
+		url: "http://www.sanluisinforma.com.ar/",
+		diario: "San Luis Informa",
 		patron: 'article',
-		patronTitulo:'h2.article-title',
-		patronImg:'.item-image img',
-		patronUrl:'a'
+		patronTitulo: 'h2.article-title',
+		patronImg: '.item-image img',
+		patronUrl: 'a'
 	},
-	lpsl:{
-		url:"http://www.lapuntasanluis.com/",
+	lpsl: {
+		url: "http://www.lapuntasanluis.com/",
 		patron: 'article',
-		patronTitulo:'h2.article-title',
-		patronImg:'img',
-		patronUrl:'a'
+		patronTitulo: 'h2.article-title',
+		patronImg: 'img',
+		patronUrl: 'a'
 	}
 }
 
 const CrawlerPromesa = (pagina) => {
-	const { url, patron, patronTitulo, patronImg, patronUrl } = diarios[pagina];
-    return new Promise((resolve, reject) => {
-        request.post(url, (err, res, body) => {
-            if (err) { reject(error); }
-            const $ = cheerio.load(body)
-            
-            const articulos = $(patron)
-        	articulos.each(function(index, el) {
-        		console.log($(el).find(patronTitulo).text().trim())
-        		console.log($(el).find(patronImg).attr('src'))
-        		console.log($(el).find(patronUrl).attr('href'))
-                console.log('====================')
-                
-                {
-                    "Noticia": [
-                        { "Diaro": pagina, "lastName": "Doe" },
-                    ]
-                }
+	const {
+		url,
+		diario,
+		patron,
+		patronTitulo,
+		patronImg,
+		patronUrl
+	} = diarios[pagina];
 
-        	});
-
-            const data = "FIN"//$(patron).text().trim()
-            resolve(data)
-        })
-    })
+	return new Promise((resolve, reject) => {
+		request.post(url, (err, res, body) => {
+			if (err) {
+				reject(error);
+			}
+			const $ = cheerio.load(body)
+			const articulos = $(patron)
+			const noticias = []
+			articulos.each((index, el) => {
+				noticias.push({
+					"Diaro": diario,
+					"Titulo": $(el).find(patronTitulo).text().trim(),
+					"Imagen": $(el).find(patronImg).attr('src'),
+					"URL": $(el).find(patronUrl).attr('href')
+				})
+			});
+			resolve(noticias)
+		})
+	})
 }
 
 CrawlerPromesa('slinforma').then(data => console.log(data)).catch(error => console.error(error))
