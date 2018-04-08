ae8a6402e649989817ce35eca5bc19778a9477e0
LcsGrz <lcsjuegos@gmail.com>
2018-04-08 17:19:18 -0300

    Guarda en BD, es un cambio brusco, olvide de subir por pequeños codigos

--- Crawlers/CR4WL3R.js
+++ Crawlers/CR4WL3R.js
@@ -1,50 +1,84 @@
-const fs = require('fs');
-const request = require('request')
-const cheerio = require('cheerio')
+const fs = require("fs")
+const request = require("request")
+const cheerio = require("cheerio")
+const mongodb = require("./mongodb/mongodb/mongodb")
+//mongod.exe --dbpath "C:\Users\LcsGrz\Documents\Universidad\3ro\LAB IV\GitLabIV\PruebaLabIV\Crawlers\db" <-- mi dir para abrir el server
 
+/*
+mongo
+show dbs
+use apitest
+show tables
+db.noticias.find().pretty()
+*/ 
+//---------------------------------------------------------------------------------------------
+//GUARDAR EN MONGODB
+module.exports = {
+  mongoUrl: "mongodb://127.0.0.1:27017/",
+  mongoDbName: "db"
+}
+
+const guardarBD = datos => {
+  datos.map(n => {
+    n.map(N => {
+      mongodb.findOne("Noticias", N).then(obj => {
+        if (obj === null) {
+          mongodb.insert("Noticias", N)
+            .then(doc => {
+              console.log(doc.result)
+            })
+            .catch(err => {
+              console.log(err)
+            })
+        } else console.log("ya existe perrin")
+      }).catch(err => console.log(err))
+    })
+  })
+}
+//---------------------------------------------------------------------------------------------
 const guardar = dato => {
   fs.writeFile("noticias.json", JSON.stringify(dato, null, 2), err => {
-    if (err) console.log("Se produjo un error al escribir.");
+    if (err) console.log("Se produjo un error al escribir.")
     else console.log("El archivo se guardo correctamente.")
   })
 }
 
 const diarios = {
   noticiasulp: {
-    url: 'http://noticias.ulp.edu.ar/php/functions/functions.php?operacion=7',
-    patron: 'div.box-noticia',
+    url: "http://noticias.ulp.edu.ar/php/functions/functions.php?operacion=7",
+    patron: "div.box-noticia",
     diario: "Noticias ULP",
-    patronTitulo: 'div#titulo-portada',
-    patronImg: 'div.imagen-noticia a img',
-    patronUrl: '#titulo-portada a'
+    patronTitulo: "div#titulo-portada",
+    patronImg: "div.imagen-noticia a img",
+    patronUrl: "#titulo-portada a"
   },
   diariosl: { //FALTA PATRONES
     url: "http://eldiariodesanluis.com/",
-    patron: 'h3 a.title'
+    patron: "h3 a.title"
   },
   slinforma: {
     url: "http://www.sanluisinforma.com.ar/",
     diario: "San Luis Informa",
-    patron: 'article',
-    patronTitulo: 'h2.article-title',
-    patronImg: '.item-image img',
-    patronUrl: 'a'
+    patron: "article",
+    patronTitulo: "h2.article-title",
+    patronImg: ".item-image img",
+    patronUrl: "a"
   },
   lpsl: {
     url: "http://www.lapuntasanluis.com/",
     diario: "La Punta San Luis",
-    patron: 'article',
-    patronTitulo: 'h2.article-title',
-    patronImg: 'img',
-    patronUrl: 'a'
+    patron: "article",
+    patronTitulo: "h2.article-title",
+    patronImg: "img",
+    patronUrl: "a"
   },
   sltv: {
     url: "http://sanluistv.com/",
     diario: "San Luis TV",
-    patron: 'div.td-block-span4',
-    patronTitulo: 'h3',
-    patronImg: 'img',
-    patronUrl: 'a'
+    patron: "div.td-block-span4",
+    patronTitulo: "h3",
+    patronImg: "img",
+    patronUrl: "a"
   }
 }
 
@@ -56,7 +90,7 @@ const CrawlerPromesa = pagina => {
     patronTitulo,
     patronImg,
     patronUrl
-  } = diarios[pagina];
+  } = diarios[pagina]
 
   return new Promise((resolve, reject) => {
     request.post(url, (err, res, body) => {
@@ -68,24 +102,25 @@ const CrawlerPromesa = pagina => {
         noticias.push({
           "Diaro": diario,
           "Titulo": $(el).find(patronTitulo).text().trim(),
-          "Imagen": $(el).find(patronImg).attr('src'),
-          "URL": $(el).find(patronUrl).attr('href')
+          "Imagen": $(el).find(patronImg).attr("src"),
+          "URL": $(el).find(patronUrl).attr("href")
         })
-      });
+      })
       resolve(noticias)
     })
   })
 }
 const crawlers = [
-  CrawlerPromesa('noticiasulp'),
-  CrawlerPromesa('slinforma'),
-  CrawlerPromesa('lpsl'),
-  CrawlerPromesa('sltv')
+  CrawlerPromesa("noticiasulp"),
+  CrawlerPromesa("slinforma"),
+  CrawlerPromesa("lpsl"),
+  CrawlerPromesa("sltv")
 ]
 
 Promise.all(crawlers).then(
-  data => guardar(data)
+  data => guardarBD(data)
 ).catch(
   error => console.log(error)
 )
+
 //CrawlerPromesa('slinforma').then(data => guardar(JSON.stringify(data,null,2))).catch(error => console.error(error))
---
+++ Crawlers/mongodb/data.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  mongoUrl: 'mongodb://127.0.0.1:27017/',
+  mongoDbName: 'apiTest'
+};
